29b4250dae20922fae8d75a547766d0f44bbf63c
Sangam Singh <hritriik@gmail.com>
2024-07-20 20:53:12 +0400

    Initial Commit

---
+++ insta/instaApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaApp.swift
+//  insta
+//
+//  Created by Sangam Singh on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct instaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
